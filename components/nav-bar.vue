f3b88a92da65e06713c1c38820b8eb2ee6182c6f
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2025-04-08 14:54:39 +0200

    removed about page
    removed about from nav links

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -2,33 +2,28 @@
 import type { NavigationMenuItem } from "@nuxt/ui";
 
 const items = ref<NavigationMenuItem[]>([
-  {
-    label: "Home",
-    icon: "material-symbols:home",
-    to: "/",
-  },
-  {
-    label: "Projects",
-    icon: "si:projects-alt-fill",
-    to: "/projects",
-  },
-  {
-    label: "About",
-    icon: "material-symbols:chat-info",
-    to: "/about",
-  },
-  {
-    label: "Contact",
-    icon: "material-symbols:contact-page-rounded",
-    to: "/contact",
-  },
+    {
+        label: "Home",
+        icon: "material-symbols:home",
+        to: "/",
+    },
+    {
+        label: "Projects",
+        icon: "si:projects-alt-fill",
+        to: "/projects",
+    },
+    {
+        label: "Contact",
+        icon: "material-symbols:contact-page-rounded",
+        to: "/contact",
+    },
 ]);
 </script>
 
 <template>
-  <div>
-    <UNavigationMenu :items="items" />
-  </div>
+    <div>
+        <UNavigationMenu :items="items" />
+    </div>
 </template>
 
 <style scoped></style>
